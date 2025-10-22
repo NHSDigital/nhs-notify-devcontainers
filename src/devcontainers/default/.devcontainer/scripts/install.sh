42a43ccee8c22a60ad150732c33cc3ba5ca0dd4e
Ross Buggins <ross.buggins@nhs.net>
2025-10-22 21:31:02 +0100

    Added all dockers

---
+++ src/devcontainers/default/.devcontainer/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Starting NHS Notify default container installation script"
+
+echo "Finished NHS Notify default container installation script"
---
+++ src/devcontainers/loaded/.devcontainer/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Starting NHS Notify loaded container installation script"
+
+echo "Finished NHS Notify loaded container installation script"
