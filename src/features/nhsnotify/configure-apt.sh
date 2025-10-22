0a39485dec5876992d08ec688f4cb385ac3b298f
Ross Buggins <ross.buggins@nhs.net>
2025-10-22 14:32:23 +0100

    configure apt install from packages.

--- src/features/nhsnotify/configure-apt.sh
+++ src/features/nhsnotify/configure-apt.sh
@@ -7,5 +7,5 @@ apt-get update
 echo "Packages to be installed:"
 cat packages.txt
 echo "Starting APT packages installation"
-cat packages.txt | xargs sudo apt-get install -y --install-suggests --install-recommends
+cat packages.txt | xargs apt-get install -y --install-suggests --install-recommends
 echo "APT packages installation complete"
