e63224890a52df54942884c7bd8e9572a77e6e0d
Ross Buggins <ross.buggins@nhs.net>
2025-11-12 17:02:19 +0000

    Codespaces version.

---
+++ src/devcontainers/loaded-codespaces/.devcontainer/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Starting NHS Notify loaded codespaces container installation script"
+
+echo "Finished NHS Notify loaded codespaces container installation script"
--- src/devcontainers/loaded-local/.devcontainer/scripts/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting NHS Notify loaded local container installation script"
-
-echo "Finished NHS Notify loaded local container installation script"
